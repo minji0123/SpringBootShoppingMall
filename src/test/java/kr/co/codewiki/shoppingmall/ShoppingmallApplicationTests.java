51b5686097858ce3cea74c8899cd3530166b17bd
minji0123 <sso02027@naver.com>
2022-02-17 20:37:22 +0900

    first commit

---
+++ src/test/java/kr/co/codewiki/shoppingmall/ShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.codewiki.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
